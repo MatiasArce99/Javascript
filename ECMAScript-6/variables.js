b4805751178dbf05c561672487e8917c658190cf
Matías <matiasarce214@gmail.com>
2023-11-06 15:51:47 -0300

    Variables locales y globales

---
+++ ECMAScript-6/variables.js
@@ -0,0 +1,32 @@
+//let -> Una varible pertenece al ámbito global o al ámbito de la función.
+let nombre = 'Matías';
+let edad = 33;
+let altura = 1.80;
+let productos = ['Yerba Mate', 'Azúcar', 'Leche', 'Galletas'];
+
+//Mostrando a través de terminal resultados
+console.log(nombre);
+console.log(edad);
+console.log(altura);
+console.log(productos);
+
+//Agrego un producto con método push
+/**
+ * Push, método que agrega elemento al final del array.
+ */
+productos.push('Fideos');
+console.log(productos);
+
+//Ámbito de una variable
+/**
+ * Variable i es global.
+ * Varibale j es local.
+ */
+let i = 0;
+function funcion() {
+    i = 1;
+    let j = 2;
+    console.log(i); //1
+    console.log(j);
+}
+funcion();
